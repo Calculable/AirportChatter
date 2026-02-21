c6a88a732233373c3174bfe38790439c013b1ec7
Jan Huber <huber@ubique.ch>
2026-02-21 08:58:27 +0100

    Initial Commit

---
+++ AirportChatter/AirportChatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportChatterApp.swift
+//  AirportChatter
+//
+//  Created by Jan Huber on 21.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct AirportChatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirportChatter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirportChatter
+//
+//  Created by Jan Huber on 21.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
